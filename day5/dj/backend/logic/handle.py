0c8d80277f499ff0b0f5c854e234c9f40c9d2372
gmkk <519518995@qq.com>
2017-10-11 23:07:51 +0800

    v0.201710112

---
+++ day5/dj/backend/logic/handle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+"""
+@version: ??
+@author: evescn
+@license: Apache Licence
+@file: handle.py
+@time: 2017/10/11 22:45
+"""
+
+def home():
+    print("welcome to home page")
+
+
+def movie():
+    print("welcome to movie page")
+
+
+def tv():
+    print("welcome to tv page")
+
+
---
+++ day5/dj/user_main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+"""
+@version: ??
+@author: evescn
+@license: Apache Licence 
+@file: user_main.py
+@time: 2017/10/11 22:48
+"""
+
+import sys
+# print(sys.path)
+from backend.logic import handle
+handle.home()
