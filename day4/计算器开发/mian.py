083c4806c60382f3882de665d9737faa6626e54d
gmkk <519518995@qq.com>
2017-10-16 23:46:51 +0800

    v0.201710162

--- day4/计算器开发/mian.py
+++ day4/计算器开发/mian.py
@@ -27,23 +27,30 @@ def chengorchu(ret1, ret2):
                 # ret = int(ret) * int(ret1[i])
                 # print(ret)
                 ret = ret / int(ret1[i+1])
-    print("ret=%d" %ret)
+    # print("ret=%d" %ret)
     return str(ret)
 
 
-def chu():
-    pass
+def jiaorjian(ret1, ret2):
+    flag = True
+    for i in range(len(ret2)):
+        if ret2[i] == '+':
+            if flag:
+                ret = int(ret1[0]) + int(ret1[1])
+                flag = False
+            else:
+                ret += int(ret1[i+1])
+        else:
+            if flag:
+                ret = int(ret1[0]) - int(ret1[1])
+                flag = False
+            else:
+                ret = ret - int(ret1[i+1])
+    # print("ret=%d" %ret)
+    return str(ret)
 
 
-def jiaorjian():
-    pass
-
-
-def jian():
-    pass
-
-
-def editstring():
+def delstring():
     pass
 
 
@@ -70,36 +77,57 @@ def stringex(string, a):
 
 
 def main():
+    # 输入字符串
     a = '1 - 2 * ( (60-30 +(-40/5) * (9-2*5/3 + 7 /3*99/4*2998 +10 * 568/14 )) - (-4*3)/ (16-3*2) )'
-    a = a.replace(" ", "")
+    a = a.replace(" ", "")   #去空格
     # while True:
+
+    # 最内行括号
     b = re.findall('\([^\(\)]+\)', a)
     for i in range(len(b)):
-        print(b[i])
-        c = re.findall("[^\(\)]+", b[i])
-        print(c)
-        for i in range(len(c)):
-            print(i)
-            d = re.split("\-|\+", c[i])
-            for i in d:
-                print(i)
-                if i == "":
-                    # print("Null")
-                    continue
-                else:
-                    ret1 = re.split("\*|\/", i)
-                    ret2 = re.findall("[\*\/]", i)
+        print("1 ", b[i])
+        if "+" in b[i] or "-" in b[i]:
+            # print("AOK")
+            if "*" in b[i] or "/" in b[i]:
+                sz = []
+                c = re.findall("[^\(\)]+", b[i])
+                print("2 %s" %c)
+                for i in range(len(c)):
+                    ret1 = re.split("\-|\+", c[i])
+                    ret2 = re.findall("\-|\+", c[i])
+                    # print("ret %s" %ret)
                     print(ret1)
                     print(ret2)
-                    if ret2:
-                        value = chengorchu(ret1, ret2)
-                        print("value=%s" % value)
-                        a = a.replace(i, value)
-                        print(a)
-
-        print(a)
-    b = re.findall('\([^\(\)]+\)', a)
-    print(b)
+                    for i in ret1:
+                        print(i)
+                        if i == "":
+                            continue
+                        # elif i != :
+                        #     sz.append(i)
+                        else:
+                            ret3 = re.split("\*|\/", i)
+                            ret4 = re.findall("[\*\/]", i)
+                            if ret4:
+                                value = chengorchu(ret3, ret4)
+                                print("value=%s" % value)
+                                sz.append(value)
+                                print("3", sz)
+                                # a = a.replace(i, value)
+                                # print(a)
+                else:
+                    if len(sz)  > len(ret2):
+                        value = jiaorjian(sz, ret2)
+                        a = a.replace(c[i], value)
+                    else:
+                        # a = a.replace(c[i], int(sz[0]))
+                        print("4 ", a)
+            else:
+                c = re.findall("[^\(\)]+", b[i])
+                for i in range(len(c)):
+                    ret1 = re.split("\-|\+", b[i])
+                # print("ret1", ret1)
+        else:
+            pass
 
 
 if __name__ == '__main__':
