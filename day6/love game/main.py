50b6852686c68a60215873b50770042ec114a313
gmkk <519518995@qq.com>
2017-11-22 23:41:25 +0800

    v0.201711222

--- day6/love game/main.py
+++ day6/love game/main.py
@@ -53,18 +53,18 @@ class Woman(Person):
 
 
     def SongWerJing(self):
-        print("送了一个围巾，亲密度增加10点。")
+        print("送了一个围巾，亲密度增加20点。")
         return 10
 
 
     def QinQin(self):
-        print("亲了一下，亲密度增加10点。")
-        return 10
+        print("亲了一下，亲密度增加40点。")
+        return 40
 
 
     def ZaoCan(self):
         print("买了一次早餐，亲密度增加10点。")
-        return 10
+        return 20
 
 
     def ShenQi(self):
@@ -81,6 +81,14 @@ def ShowInfo(key):
         '''
         print("\033[1;;33m%s\033[0m" %info)
         return input("请输入你的选择：")
+    elif key == "error":
+        info = '''
+=====================================================
+        你的输入不正确，请重新输入
+=====================================================
+             '''
+        print("\033[1;;33m%s\033[0m" % info)
+        # return input("请输入角色名字：")
     elif key == "role":
         info = '''
 =====================================================
@@ -94,48 +102,122 @@ evescn：男性角色    hlr：女性角色    q：退出游戏
         info = '''
 =====================================================
                 请选择游戏方式
-1、自动游戏    2、手动游戏    3、退出游戏
+1、自动游戏    2、手动游戏    q、退出游戏
 =====================================================
         '''
         print("\033[1;;33m%s\033[0m" % info)
         return input("请选择游戏方式：")
+    elif key == "manway":
+        info = '''
+=====================================================
+                 请选择游戏方式
+1、送一朵花    
+2、打一次水
+3、买一次早餐
+4、骂女朋友  
+q、退出游戏
+=====================================================
+        '''
+        print("\033[1;;33m%s\033[0m" % info)
+        return input("请选择你的行为：")
+    elif key == "womanway":
+        info = '''
+=====================================================
+                 请选择游戏方式
+1、送一个围巾    
+2、亲一下
+3、买一次早餐
+4、生气了  
+q、退出游戏
+=====================================================
+        '''
+        print("\033[1;;33m%s\033[0m" % info)
+        return input("请选择你的行为：")
+
+def Mangame(num):
+    if num == 1:
+        ret = evescn.SongHua()
+        hlr.Add_Intimacy(ret)
+    elif num == 2:
+        ret = evescn.DaShui()
+        hlr.Add_Intimacy(ret)
+    elif num == 3:
+        ret = evescn.ZaoCan()
+        hlr.Add_Intimacy(ret)
+    elif num == 4:
+        ret = evescn.MaRen()
+        hlr.Add_Intimacy(ret)
+
+
+def Womangame(num):
+    if num == 1:
+        ret = hlr.SongWerJing()
+        evescn.Add_Intimacy(ret)
+    elif num == 2:
+        ret = hlr.QinQin()
+        evescn.Add_Intimacy(ret)
+    elif num == 3:
+        ret = hlr.SongWerJing()
+        evescn.Add_Intimacy(ret)
+    elif num == 4:
+        ret = hlr.ZaoCan()
+        evescn.Add_Intimacy(ret)
 
 
 def autogame(name):
     day = 1
-    if name == "evescn":
-        while int(hlr.intimacy) > 0 and int(hlr.intimacy) < 100:
-            num = random.randint(1, 4)
-            print("第【%s】天" %day, end="")
-            day += 1
-            # print(hlr.intimacy)
-
-            if num == 1:
-                ret = evescn.SongHua()
-                hlr.Add_Intimacy(ret)
-            elif num == 2:
-                ret = evescn.DaShui()
-                hlr.Add_Intimacy(ret)
-            elif num == 3:
-                ret = evescn.ZaoCan()
-                hlr.Add_Intimacy(ret)
-            elif num == 4:
-                ret = evescn.MaRen()
-                hlr.Add_Intimacy(ret)
-            print()
+    while int(hlr.intimacy) > 0 and int(hlr.intimacy) < 100 and int(evescn.intimacy) > 0 and int(evescn.intimacy) < 100:
+        num = random.randint(1, 4)
+        print("第【%s】天" %day, end="")
+        day += 1
+        if name == "evescn":
+            Mangame(num)
         else:
-            if int(hlr.intimacy) <= 0:
-                print("\033[1;;31m恋爱失败！\033[0m")
-                hlr.intimacy = 10
-            else:
-                print("\033[1;;31m恋爱成功！\033[0m")
-                hlr.intimacy = 10
-    elif name == "hlr":
-        print("sb")
+            Womangame(num)
+        print()
+    else:
+        if int(hlr.intimacy) <= 0 or int(evescn.intimacy) <= 0:
+            print("\033[1;;31m恋爱失败！\033[0m")
+            hlr.intimacy = 10
+            evescn.intimacy = 10
+        else:
+            print("\033[1;;31m恋爱成功！\033[0m")
+            hlr.intimacy = 10
+            evescn.intimacy = 10
 
 
 def game(name):
-    pass
+    day = 1
+    while int(hlr.intimacy) > 0 and int(hlr.intimacy) < 100 and int(evescn.intimacy) > 0 and int(evescn.intimacy) < 100:
+        if name == "evescn":
+            num = ShowInfo("manway")
+            num = int(num)
+            if num < 1 or num > 4:
+                ShowInfo("error")
+            else:
+                print("第【%s】天" % day, end="")
+                day += 1
+                Mangame(int(num))
+                print()
+        else:
+            num = ShowInfo("womanway")
+            num = int(num)
+            if num < 1 or num > 4:
+                ShowInfo("error")
+            else:
+                print("第【%s】天" % day, end="")
+                day += 1
+                Womangame(int(num))
+                print()
+    else:
+        if int(hlr.intimacy) <= 0 or int(evescn.intimacy) <= 0:
+            print("\033[1;;31m恋爱失败！\033[0m")
+            hlr.intimacy = 10
+            evescn.intimacy = 10
+        else:
+            print("\033[1;;31m恋爱成功！\033[0m")
+            hlr.intimacy = 10
+            evescn.intimacy = 10
 
 
 def main():
@@ -150,21 +232,36 @@ def main():
 
             flag = True
             while flag:
-                if game_role == "evescn":
-                    flag = False
-                elif game_role == "hlr":
+                if game_role == "evescn" or game_role == "hlr":
                     flag = False
                 else:
-                    print("输入的角色名不正确，请重新输入名字")
+                    # print()
+                    # print("\033[1;;35m====================================================\033[0m")
+                    # print("输入的角色名不正确，请重新输入名字")
+                    # print("\033[1;;35m====================================================\033[0m")
+                    ShowInfo("error")
                     game_role = ShowInfo("role")
 
             game_way = ShowInfo("way")
-            if game_way == "2":
+            if game_way == "q":
                 sys.exit("欢迎下次再来！")
             elif game_way == "1":
                 autogame(game_role)
             elif game_way == "2":
                 game(game_role)
+            else:
+                print()
+                print("\033[1;;35m====================================================\033[0m")
+                print("\033[31m你的游戏方式不正确\033[0m")
+                print("\033[1;;35m====================================================\033[0m")
+
+        else:
+            # print()
+            # print("\033[1;;35m====================================================\033[0m")
+            # print("\033[32m不知道你的选择是什么，请重新输入\033[0m")
+            # print("\033[1;;35m====================================================\033[0m")
+            ShowInfo("error")
+            ret = ShowInfo("begin")
 
 
 if __name__ == '__main__':
---
+++ day7/多态-lx2.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/11/22 23:20
+# @Author  : Evescn
+# @Site    : 
+# @File    : 多态-lx2.py
+# @Software: PyCharm
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/11/22 23:08
+# @Author  : Evescn
+# @Site    :
+# @File    : 多态.py
+# @Software: PyCharm
+
+# _*_coding:utf-8_*_
+
+
+class Animal(object):
+    def __init__(self, name):  # Constructor of the class
+        self.name = name
+
+    def talk(self):  # Abstract method, defined by convention only
+        raise NotImplementedError("Subclass must implement abstract method")
+
+
+class Cat(Animal):
+    def talk(self):
+        print('%s: 喵喵喵!' % self.name)
+        # return ('喵喵喵!')
+
+
+class Dog(Animal):
+    def talk(self):
+        print('%s: 汪！汪！汪！' % self.name)
+        # return ('汪！汪！汪！')
+
+
+animals = [Cat("Evescn"),
+           Dog("Gmkk")]
+
+for animal in animals:
+    print(animal.talk())
