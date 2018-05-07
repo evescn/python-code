119803f0e70bfd2166c20addc154889488610d35
evescn <519518995@qq.com>
2018-05-07 18:27:41 +0800

    v0.20180507

---
+++ day9/gevent-lx1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/5/4 15:44
+# @Author  : Evescn
+# @Site    : 
+# @File    : gevent-lx1.py
+# @Software: PyCharm
+
+import gevent, threading
+
+def task(pid):
+    """
+    Some non-deterministic task
+    :param pid:
+    :return:
+    """
+    gevent.sleep(0.5)
+    print('Task %s done' % pid)
+
+def synchronous():
+    for i in range(1, 10):
+        task(i)
+
+def asynchronous():
+    threads = [gevent.spawn(task, i) for i in range(10)]
+    gevent.joinall(threads)
+
+print('Synchronous:')
+synchronous()
+
+print('Asynchronous:')
+asynchronous()
--- day9/q_task_done.py
+++ day9/q_task_done.py
@@ -8,38 +8,75 @@
 
 import threading, queue, time, random
 
-def producer(name):
-    count = 1
-    # time.sleep(random.randint(1,5))
-    while True:
-        time.sleep(random.random())
-        if q.qsize() < 2:
-            print("[%s] 制作的包子 [%s]" %(name, count))
-            q.put(count)
-            count += 1
-            q.join()
-            print("[%s] 制作的所以的包子被领取了" %name )
 
-def consumer(name):
-    while True:
-        print("[%s] 吃了第 [%s] 个包子" %(name, q.get()))
-        time.sleep(1)
-        q.task_done()
+# def closeBlcok(n):
+#     def sum():
+#         return n * n
+#     return sum
+#
+#
+# closeFunction = closeBlcok(10)
+#
+# aa = closeFunction()
+#
+# print(aa)
 
-q = queue.Queue()
 
-c1 = threading.Thread(target=consumer, args=["c1",])
-c2 = threading.Thread(target=consumer, args=["c2",])
-# c3 = threading.Thread(target=consumer, args=["c3",])
+def count():
+    fs = []
+    for i in range(1, 4):
+        def f():
+             return i*i
+        fs.append(f)
+    print(len(fs))
+    print(fs)
+    return fs
 
-p1 = threading.Thread(target=producer, args=["p1",])
-p2 = threading.Thread(target=producer, args=["p2",])
-p3 = threading.Thread(target=producer, args=["p3",])
+f1, f2, f3 = count()
+print(type(f1))
+print(f1())
+print(f2())
+print(f3())
 
-c1.start()
-c2.start()
-# c3.start()
+z = [1, 2, 4, 7]
 
-p1.start()
-p2.start()
-p3.start()
+a, b, *c = z
+print(a)
+print(b)
+print(c)
+
+# def producer(name):
+#     count = 1
+#     # time.sleep(random.randint(1,5))
+#     while True:
+#         time.sleep(random.random())
+#         if q.qsize() < 2:
+#             print("[%s] 制作的包子 [%s]" %(name, count))
+#             q.put(count)
+#             count += 1
+#             q.join()
+#             print("[%s] 制作的所以的包子被领取了" %name )
+#
+# def consumer(name):
+#     while True:
+#         print("[%s] 吃了第 [%s] 个包子" %(name, q.get()))
+#         time.sleep(1)
+#         q.task_done()
+#
+# q = queue.Queue()
+#
+# c1 = threading.Thread(target=consumer, args=["c1",])
+# c2 = threading.Thread(target=consumer, args=["c2",])
+# # c3 = threading.Thread(target=consumer, args=["c3",])
+#
+# p1 = threading.Thread(target=producer, args=["p1",])
+# p2 = threading.Thread(target=producer, args=["p2",])
+# p3 = threading.Thread(target=producer, args=["p3",])
+#
+# c1.start()
+# c2.start()
+# # c3.start()
+#
+# p1.start()
+# p2.start()
+# p3.start()
