b77ca46bd89ad302b38b59e663c95de5de825a1b
gmkk <519518995@qq.com>
2018-05-30 00:07:45 +0800

    v0.20180529

---
+++ pachong/shizhan/jiepai/fengjing/config.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/5/29 22:59
+# @Author  : Evescn
+# @Site    : 
+# @File    : config.py
+# @Software: PyCharm
+
+MONGO_URL='localhost'
+MONGO_DB='TouTiao'
+MONGO_TABLE='toutiao'
+
+GROUP_START=0
+GROUP_END=20
+
+KEYWORD='街拍'
--- pachong/shizhan/jiepai/fengjing/spider.py
+++ pachong/shizhan/jiepai/fengjing/spider.py
@@ -1,39 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @Time    : 18-5-29 下午4:18
+# @Time    : 2018/5/29 21:42
 # @Author  : Evescn
 # @Site    : 
-# @File    : spider.py
-# @Software: PyCharm Community Edition
+# @File    : spider.py.py
+# @Software: PyCharm
 import json
+
 import os
-from hashlib import md5
+import pymongo
 import re
+import requests
+from requests.exceptions import RequestException
 from urllib.parse import urlencode
 from bs4 import BeautifulSoup
-from requests.exceptions import RequestException
-import requests
 from config import *
-import pymongo
+from hashlib import md5
 from multiprocessing import Pool
 
+client = pymongo.MongoClient('MONGO_URL')
+db = client['MONGO_DB']
 
-client = pymongo.MongoClient(MONGO_URL, connect=False)
-db = client[MONGO_DB]
-
-
-
-def get_page_index(offset, keyword):
+def get_page_index(offset):
     data = {
         'offset': offset,
         'format': 'json',
-        'keyword': keyword,
+        'keyword': KEYWORD,
         'autoload': 'true',
         'count': '20',
-        'cur_tab': 3
+        'cur_tab': 1,
+        'from': 'gallery'
     }
 
-    url = 'http://www.toutiao.com/search_content/?' +urlencode(data)
+    url = 'https://www.toutiao.com/search_content/?' + urlencode(data)
 
     try:
         response = requests.get(url)
@@ -41,15 +40,17 @@ def get_page_index(offset, keyword):
             return response.text
         return None
     except RequestException:
-        print("请求索引页出错")
+        print('请求索引页错误......')
         return None
 
 
 def parse_page_index(html):
     data = json.loads(html)
+    # print(type(data))
+    # print(data)
     if data and 'data' in data.keys():
         for item in data.get('data'):
-            yield  item.get('article_url')
+            yield item.get('article_url')
 
 
 def get_page_detail(url):
@@ -62,26 +63,33 @@ def get_page_detail(url):
             return response.text
         return None
     except RequestException:
-        print("请求详细页出错")
+        print('请求索引页错误......')
         return None
 
 
 def parse_page_detail(html, url):
-    print(html)
+    # print(html)
+    # soup = BeautifulSoup(html, 'lxml')
+    # title = soup.select('title')[0].get_text()
     soup = BeautifulSoup(html, 'lxml')
     title = soup.select('title')[0].get_text()
     print(title)
-    images_pattern = re.compile('gallery: JSON.parse\((.*?)\),', re.S)
-    result  = re.search(images_pattern, html)
+    pattern = re.compile('gallery: JSON.parse\((.*?)\),', re.S)
+    result = re.search(pattern, html)
     if result:
-        print(type(result.group(1)))
         data = json.loads(result.group(1))
         data = json.loads(data)
         print(type(data))
         if data and 'sub_images' in data.keys():
+            # for image in data.get('sub_images'):
+            #     yield {
+            #         'title': title,
+            #         'url': url,
+            #         'image': image
+            #     }
             sub_images = data.get('sub_images')
             images = [item.get('url') for item in sub_images]
-            # for images in images: download_image(images)
+            for images in images: download_image(images)
             return {
                 'title': title,
                 'url': url,
@@ -89,8 +97,8 @@ def parse_page_detail(html, url):
             }
 
 
-def save_to_mongo(result):
-    if db[MONGO_TABLE].insert(result):
+def save_to_mongodb(result):
+    if db['MONGO_TABLE'].insert(result):
         print('存储到MongoDB成功', result)
         return True
     return False
@@ -110,26 +118,48 @@ def download_image(url):
         print("请求图片出错")
         return None
 
+
 def save_image(content):
-    file_path = '{0}/{1}.{2}'.format(os.getcwd(), md5(content).hexdigest(), 'jpg')
+    file_path = '{0}/tp/{1}.{2}'.format(os.getcwd(), md5(content).hexdigest(), 'jpg')
     if not os.path.exists(file_path):
         with open(file_path, 'wb') as f:
             f.write(content)
             f.close()
 
-
 def main(offset):
-    html = get_page_index(offset, KEYWORD)
+    html = get_page_index(offset)
+    # print(html)
     for url in parse_page_index(html):
-        html = get_page_detail(url)
-        if html:
-            result = parse_page_detail(html, url)
-            print(result)
-            # if result: save_to_mongo (result)
+        print(url)
+        if url:
+            html = get_page_detail(url)
+        # print(html)
+            if html:
+                image = parse_page_detail(html, url)
+                # print(image)
+
 
 
 if __name__ == '__main__':
     # main()
+    # groups = [i*20 for i in range(GROUP_START, GROUP_END)]
+    # pool = Pool()
+    # pool.map(main, groups)
+
     groups = [x * 20 for x in range(GROUP_START, GROUP_END+1)]
     pool = Pool()
     pool.map(main, groups)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
