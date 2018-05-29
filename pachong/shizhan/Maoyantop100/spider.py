38fd109bcbe07fdcf617aec6acd0f909fb96f0c9
gmkk <519518995@qq.com>
2018-05-29 14:54:26 +0800

    v0.20180528

---
+++ pachong/shizhan/Maoyantop100/spider.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/5/28 22:25
+# @Author  : Evescn
+# @Site    : 
+# @File    : spider.py.py
+# @Software: PyCharm
+import json
+from multiprocessing import Pool
+import requests
+from requests.exceptions import RequestException
+import re
+
+
+def get_one_page(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'
+    }
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            return response.text
+        return None
+    except RequestException:
+        return None
+
+
+def parse_one_page(html):
+    pattern = re.compile('<dd>.*?board-index.*?>(\d+)</i>.*?title="(.*?)".*?data'
+                         +'-src="(.*?)".*?star">(.*?)</p>.*?time">(.*?)</p.*?int'
+                         +'eger">(.*?)</i.*?tion">(.*?)</i>.*?</dd>', re.S)
+    # pattern = re.compile('<dd>.*?board-index.*?">(\d+)</i>.*?data-src="(.*?)".*?s'
+    #                      +'tar">(.*?)</p>.*?time">(.*?)</p.*?integer">(.*?)</i>.*?t'
+    #                      +'ion">(.*?)</i>.*?</dd>', re.S)
+    items = re.findall(pattern, html)
+    for item in items:
+        yield {
+            'index': item[0],
+            'title': item[1],
+            'image': item[2],
+            'actor': item[3].strip()[3:],
+            'time': item[4].strip()[5:],
+            'score': item[5]+item[6]
+        }
+
+
+def write_to_file(content):
+    with open('result.txt', 'a', encoding='utf-8') as f:
+        f.write(json.dumps(content, ensure_ascii=False) + '\n')
+        f.close()
+
+
+def main(offset):
+    url = 'http://maoyan.com/board/4?offset=' + str(offset)
+    html = get_one_page(url)
+    # print(html)
+    for item in parse_one_page(html):
+        print(item)
+        write_to_file(item)
+
+if __name__ == '__main__':
+    # for i in range(10):
+    #     main(i*10)
+    pool = Pool()
+    pool.map(main, [i*10 for i in range(10)])
